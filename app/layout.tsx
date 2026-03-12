c59cddb856b8a23090487f9fc2064feb6a41d5d3
Junaid <jgnd95@gmail.com>
2026-03-13 00:43:33 +0100

    Ios zoom fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,12 @@ export const metadata: Metadata = {
   description: "Turning ideas into reality",
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
